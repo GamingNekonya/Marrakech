b8d9fc83e69319f4e976a474a7d6b060aba27812
Qihang Xu <u6442026@anu.edu.au>
2023-09-14 16:27:49 +1000

    Board set and assam's position initialised.

--- src/comp1110/ass2/Board.java
+++ src/comp1110/ass2/Board.java
@@ -4,16 +4,43 @@ package comp1110.ass2;
  * Represents the game board of Marrakech.
  */
 public class Board {
+    private int[][] assamBoard;
+    private int[][] rugBoard;
+    private static final int BOARD_SIZE = 7;
     //2D array representing the board.
     /**
      * Constructor to initialize the game board.
      */
-    private Rug[][] board;
-    //initialized the board, all position should be empty
     public Board(){
+        assamBoard = new int[BOARD_SIZE][BOARD_SIZE];
+        rugBoard = new int[BOARD_SIZE][BOARD_SIZE];
         //Sample initialization....
     }
 
+    private void inBoards(){
+        for (int i = 0; i <BOARD_SIZE; i++){
+            for (int j = 0; j < BOARD_SIZE; j++){
+                assamBoard[i][j] = 0;
+                rugBoard[i][j] = 0;
+            }
+        }
+    }
+
+    /**
+     * place the assam in a position and clear the old position
+     * @param x
+     * @param y
+     */
+    public void placeAssam(int x, int y){
+        for (int i = 0; i < BOARD_SIZE; i++){
+            for(int j = 0; j < BOARD_SIZE; j++){
+                if(assamBoard[i][j] == 1){
+                    assamBoard[i][j] = 0;
+                }
+            }
+        }
+        assamBoard[x][y] = 1;
+    }
     /**
      * Places a rug on the board
      *
