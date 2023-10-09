011b2811029fdbb31fc14e1dabbd44540a7d1117
Tianze Bai <u7136753@amu.edu.au>
2023-10-09 11:04:58 +1100

    viewer change

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -140,6 +140,44 @@ public class Viewer extends Application {
         boardGroup.getChildren().addAll(assamCircle, orientationLine);
     }
 
+    private void displayPlayerInfo(Player player, int index) {
+        double startX = 400;
+        double startY = 50 + index * 150;
+
+        Rectangle playerBox = new Rectangle(startX-10 , startY-20, 200, 100);
+        playerBox.setFill(Color.WHITE);
+        playerBox.setStroke(Color.BLACK);
+        playerBox.setStrokeWidth(2);
+
+        Label titleLabel = new Label("Player " + (index + 1));
+        titleLabel.setLayoutX(startX + 20);
+        titleLabel.setLayoutY(startY - 20);
+
+        Rectangle colorBox = new Rectangle(startX, startY, 40, 60);
+        switch (player.getColor()) {
+            case 'c': colorBox.setFill(Color.CYAN); break;
+            case 'y': colorBox.setFill(Color.YELLOW); break;
+            case 'r': colorBox.setFill(Color.RED); break;
+            case 'p': colorBox.setFill(Color.PURPLE); break;
+        }
+
+
+        Label rugsLabel = new Label("Rugs: " + player.getRugs());
+        rugsLabel.setLayoutX(startX + 50);
+        rugsLabel.setLayoutY(startY + 25);
+
+        Label dirhamsLabel = new Label("Dirhams: " + player.getDirhams());
+        dirhamsLabel.setLayoutX(startX + 50);
+        dirhamsLabel.setLayoutY(startY);
+
+
+        Label inGameLabel = new Label(player.isInGame() ? "In Game" : "Out of Game");
+        inGameLabel.setLayoutX(startX + 50);
+        inGameLabel.setLayoutY(startY + 50);
+
+        playerInfoGroup.getChildren().addAll(playerBox, titleLabel, colorBox, dirhamsLabel, rugsLabel, inGameLabel);
+    }
+
 
 
     /**
